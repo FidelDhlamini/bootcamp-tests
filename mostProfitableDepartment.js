48f937a3a45b2b22645bf8fb70c9b6f4132bfee6
FidelDhlamini <goodkidfidel@gmail.com>
2019-05-21 15:57:35 +0200

    added mostProfitableDepartment

---
+++ mostProfitableDepartment.js
@@ -0,0 +1,17 @@
+var mostProfitableDepartment = function (str) {
+    console.log(str);
+    var highSale = 0;
+    var answer = [];
+    for(var i = 0; i < str.length; i++)
+    {
+      var sale = str[i].sales;
+      if(sale > highSale)
+      {
+        highSale = sale;
+        answer = str[i].department;
+      }
+    }
+    return answer;
+  }
+  
+  
