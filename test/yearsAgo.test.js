ada39c1d4090a55ea555905874455529dac9d882
FidelDhlamini <goodkidfidel@gmail.com>
2019-05-27 12:01:52 +0200

    changed descriptions

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,12 +1,12 @@
 describe('The yearsAgo function', function(){
 
-    it('it should return how many years ago the year entered was', function(){
+    it('it should return how many years ago the year 2000 was', function(){
         assert.equal(yearsAgo(2000), 19);    
     });
-    it('it should return how many years ago the year entered was', function(){
+    it('it should return how many years ago the year 1996 was', function(){
         assert.equal(yearsAgo(1996), 23);    
     });
-    it('it should return how many years ago the year entered was', function(){
+    it('it should return how many years ago the year 1994 was', function(){
         assert.equal(yearsAgo(1994), 25);    
     });
    
